44ae260a40bfc2a5e0f2d004ceaced3bddbf7bae
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-05-04 11:33:28 +0200

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,32 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Products from "./pages/Products/Products";
+import Product from "./pages/Product/Product";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+
+const Layout = () => {
+  return (
+    <div className="app">
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  )
+}
 
 const router = createBrowserRouter([
   {
     path:"/",
-    element:<span>Home</span>
+    element:<Home />
   },
   {
     path:"/products/:id",
-    element:<span>Category</span>
+    element:<Products />
   },
   {
     path:"/product/:id",
-    element:<span>Product</span>
+    element:<Product />
   },
 ])
 
